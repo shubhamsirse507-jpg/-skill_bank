c0d6b32e28b0d476bf2735a30a9b3984508f3e8a
shubhamsirse507-jpg <shubhamsirse507@gmail.com>
2026-08-04 16:15:42 +0530

    Delete authentication directory

--- authentication/constants/assets.py
+++
@@ -1,8 +0,0 @@
-# Media & Asset URLs Configuration
-# Contains direct links to external video and image assets used across authentication pages.
-
-AUTH_BACKGROUND_VIDEO_URL = "https://videos.pexels.com/video-files/8675543/8675543-hd_1920_1080_30fps.mp4"
-
-# Skill Bank Lightbulb/Puzzle Logo Image
-BRAND_LOGO_IMAGE_NAME = "academic.png"
-
